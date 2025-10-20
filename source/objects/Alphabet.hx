7c5cadb2a9d841076e1ee2b8236d1f78208161dd
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-10-21 01:29:36 +0200

    Update Alphabet.hx

--- source/objects/Alphabet.hx
+++ source/objects/Alphabet.hx
@@ -528,3 +528,10 @@ class AlphaCharacter extends FlxSprite
 		updateLetterOffset();
 	}
 }
+
+function boundTo(value:Float, min:Float, max:Float):Float {
+	var newValue:Float = value;
+	if(newValue < min) newValue = min;
+	else if(newValue > max) newValue = max;
+	return newValue;
+}

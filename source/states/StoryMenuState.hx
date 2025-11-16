e690dc3414f116819a673610b6933fba4b4a88bc
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-11-16 14:16:10 +0100

    Update StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -350,7 +350,7 @@ class StoryMenuState extends MusicBeatState
 
 			new FlxTimer().start(1, function(tmr:FlxTimer)
 			{
-				LoadingState.prepareToSong();
+				//LoadingState.prepareToSong();
 				LoadingState.loadAndSwitchState(new PlayState(), true);
 				FreeplayState.destroyFreeplayVocals();
 			});

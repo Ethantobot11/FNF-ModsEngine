e1c493c7e5e2709e4c1d96e99503e18145080a3d
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-11-16 13:11:47 +0100

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2568,6 +2568,7 @@ class PlayState extends MusicBeatState
 					PlayState.SONG = Song.loadFromJson(PlayState.storyPlaylist[0] + difficulty, PlayState.storyPlaylist[0]);
 					FlxG.sound.music.stop();
 
+					LoadingState.prepareToSong();
 					LoadingState.loadAndSwitchState(new PlayState());
 				}
 			}

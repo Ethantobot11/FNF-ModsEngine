1f02cbebabca04b31a048c1a87b57be5586172e2
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-11-16 14:19:35 +0100

    Update CoolUtil.hx

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -142,24 +142,6 @@ class CoolUtil
 		return '${company}/${flixel.util.FlxSave.validate(FlxG.stage.application.meta.get('file'))}';
 		// #end
 	}
-
-	public static function loadSong(?name:String = null, ?difficultyNum:Int = -1)
-	{
-		if (name == null || name.length < 1)
-			name = PlayState.SONG.song;
-		if (difficultyNum == -1)
-			difficultyNum = PlayState.storyDifficulty;
-
-		var poop:String = Highscore.formatSong(name, difficultyNum);
-		PlayState.SONG = Song.loadFromJson(poop, name);
-		PlayState.storyDifficulty = difficultyNum;
-		LoadingState.prepareToSong();
-		LoadingState.loadAndSwitchState(new PlayState());
-
-		// FlxG.sound.music.pause();
-		// FlxG.sound.music.volume = 0;
-	}
-
 	public static function setTextBorderFromString(text:FlxText, border:String)
 	{
 		switch(border.toLowerCase().trim())

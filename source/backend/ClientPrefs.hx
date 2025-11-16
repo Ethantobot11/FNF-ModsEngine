ef4ca70475e6fbcb2113f7cc1bf60377a7f1d4c3
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-11-16 11:31:49 +0100

    Update ClientPrefs.hx

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -92,6 +92,7 @@ import states.TitleState;
 	public var safeFrames:Float = 10;
 	public var guitarHeroSustains:Bool = false;
 	public var discordRPC:Bool = true;
+	public var loadingScreen:Bool = true;
 }
 
 class ClientPrefs {

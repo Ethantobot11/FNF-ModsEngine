7e58679c1fd8d7edbd2a8d744a63b08512a016da
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-11-22 20:58:15 +0100

    Update Song.hx

--- source/backend/Song.hx
+++ source/backend/Song.hx
@@ -131,7 +131,26 @@ class Song
 
 		return rawJson;
 	}
-	return parseRawJSON(jsonInput, loadRawSong(jsonInput, folder));
+
+	public static function loadFromJson(jsonInput:String, ?folder:String):SwagSong
+	{
+		// FIX THE CASTING ON WINDOWS/NATIVE
+		// Windows???
+		// trace(songData);
+
+		// trace('LOADED FROM JSON: ' + songData.notes);
+		/* 
+			for (i in 0...songData.notes.length)
+			{
+				trace('LOADED FROM JSON: ' + songData.notes[i].sectionNotes);
+				// songData.notes[i].sectionNotes = songData.notes[i].sectionNotes
+			}
+
+				daNotes = songData.notes;
+				daSong = songData.song;
+				daBpm = songData.bpm; */
+
+		return parseRawJSON(jsonInput, loadRawSong(jsonInput, folder));
 	}
 
 	public static function parseRawJSON(jsonInput:String, rawSONG:String) {
@@ -174,63 +193,7 @@ class Song
 		throw new haxe.Exception("No song data found, or is invalid.");
 	}
 
-	public static function loadFromJson(jsonInput:String, ?folder:String):SwagSong
-	{
-		var rawJson = null;
-		
-		var formattedFolder:String = Paths.formatToSongPath(folder);
-		var formattedSong:String = Paths.formatToSongPath(jsonInput);
-		#if MODS_ALLOWED
-		var moddyFile:String = Paths.modsJson(formattedFolder + '/' + formattedSong);
-		if(FileSystem.exists(moddyFile)) {
-			rawJson = File.getContent(moddyFile).trim();
-		}
-		#end
-
-		if(rawJson == null) {
-			var path:String = Paths.json(formattedFolder + '/' + formattedSong);
-
-			#if sys
-			if(FileSystem.exists(path))
-				rawJson = File.getContent(path).trim();
-			else
-			#end
-				rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();
-		}
-
-		while (!rawJson.endsWith("}"))
-		{
-			rawJson = rawJson.substr(0, rawJson.length - 1);
-			// LOL GOING THROUGH THE BULLSHIT TO CLEAN IDK WHATS STRANGE
-		}
-
-		// FIX THE CASTING ON WINDOWS/NATIVE
-		// Windows???
-		// trace(songData);
-
-		// trace('LOADED FROM JSON: ' + songData.notes);
-		/* 
-			for (i in 0...songData.notes.length)
-			{
-				trace('LOADED FROM JSON: ' + songData.notes[i].sectionNotes);
-				// songData.notes[i].sectionNotes = songData.notes[i].sectionNotes
-			}
-
-				daNotes = songData.notes;
-				daSong = songData.song;
-				daBpm = songData.bpm; */
-
-		var songJson:Dynamic = parseJSONshit(rawJson);
-		if(jsonInput != 'events') StageData.loadDirectory(songJson);
-		onLoadJson(songJson);
-		return songJson;
-	}
-
-	public static function parseJSONshit(rawJson:String):SwagSong
-	{
-		return cast Json.parse(rawJson).song;
-	}
-    public static function updateManiaKeys(songData:SwagSong):Int {
+	public static function updateManiaKeys(songData:SwagSong):Int {
 		var keys = null;
 
 		if (songData.mania != null)
